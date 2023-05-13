07d7f62dcc2591c9fd1e1c86cd4117ea1e448c7b
Nita Cosmin <102913196+NitaCosmin@users.noreply.github.com>
2023-05-13 11:44:14 +0300

    TEst

---
+++ Preg/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Preg
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            System.IO.File.WriteAllText("output.txt",string.Empty);
+
+            var linie = File.ReadAllLines("input.txt").ToList();
+            var nr = Int32.Parse(linie[0]);
+            var numere = linie[1].Split(' ').Select(Int32.Parse).ToList();
+
+            int[] Vect=new int[nr];
+
+            for (int i=0;i<nr;i++)
+            {
+                Vect[i] = numere[i];
+            }
+
+            
+
+             int[] LIS(int[] a, int n)
+            {
+                int[] lung = new int[n];
+                lung[n - 1] = 1;
+
+                for (int i = n - 2; i >= 0; i--)
+                {
+                    int max = 0;
+                    for (int j = i + 1; j < n; j++)
+                    {
+                        if (a[i] <= a[j])
+                        {
+                            if (max < lung[j])
+                            {
+                                max = lung[j];
+                            }
+                        }
+                    }
+                    lung[i] = max + 1;
+                }
+
+                return lung;
+            }
+
+           
+             void TipLIS(int[] a, int n, int[] lung)
+            {
+                int max = lung[0];
+                int poz = 0;
+
+                for (int i = 1; i < n; i++)
+                {
+                    if (max < lung[i])
+                    {
+                        max = lung[i];
+                        poz = i;
+                    }
+                }
+
+                Console.WriteLine("Lungimea maximă este " + max);
+                Console.WriteLine("Iar subsecvenţa este:");
+
+                Console.Write(a[poz] + " ");
+                for (int i = poz + 1; i < n; i++)
+                {
+                    if (lung[i] == max - 1 && a[i] >= a[poz])
+                    {
+                        Console.Write(a[i] + " ");
+                        poz = i;
+                        max--;
+                    }
+                }
+
+                Console.WriteLine();
+            }
+
+            var raspuns = LIS(Vect, nr);
+            TipLIS(Vect,nr, raspuns);
+            File.AppendAllText("output.txt", string.Join(" ", raspuns));
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ PregLCS/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PregLCS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            System.IO.File.WriteAllText("output.txt", string.Empty);
+            var line = File.ReadLines("input.txt").ToList();
+            var nr = Int32.Parse(line[0]);
+            var numere = line[1].Split(' ').Select(Int32.Parse).ToList();
+
+            int[] vector = new int[nr];
+            for (int i = 0; i < nr; i++)
+            {
+                vector[i] = numere[i];
+            }
+            var nr1 = Int32.Parse(line[2]);
+            var numere1 = line[3].Split(' ').Select(Int32.Parse).ToList();
+
+            int[] vector1 = new int[nr1];
+            for (int i = 0; i < nr1; i++)
+            {
+                vector1[i] = numere1[i];
+            }
+
+            int[,] LCS(int[] a, int[] b)
+            {
+                int n = a.Length, m = b.Length;
+                int[,] lung = new int[n, m];
+                lung[0, 0] = (a[0] == b[0]) ? 1 : 0;
+                for (int j = 1; j < m; j++)
+                    lung[0, j] = (a[0] == b[j]) ? 1 : lung[0, j - 1];
+                for (int i = 1; i < n; i++)
+                    lung[i, 0] = (a[i] == b[0]) ? 1 : lung[i - 1, 0];
+                for (int i = 1; i < n; i++)
+                    for (int j = 1; j < m; j++)
+                        lung[i, j] = Math.Max(
+                        (a[i] == b[j]) ? lung[i - 1, j - 1] + 1 : lung[i - 1, j - 1],
+                        Math.Max(lung[i - 1, j], lung[i, j - 1])
+                        );
+                return lung;
+            }
+            /// <summary>
+            /// Tipareste subsecventa crescatoare comuna subsirurilor a si b.
+            /// </summary>
+            void TipLCS(int[] a, int[] b, int[,] lung)
+            {
+                List<int> subsecventa = new List<int>();
+                int i = a.Length - 1;
+                int j = b.Length - 1;
+                while (i >= 0 && j >= 0)
+                {
+                    if (a[i] == b[j])
+                    {
+                        subsecventa.Add(a[i]);
+                        i--; j--;
+                    }
+                    else
+                    if (i >= 1 && lung[i - 1, j] == lung[i, j]) i--;
+                    else if (j >= 1 && lung[i, j - 1] == lung[i, j]) j--;
+                }
+                subsecventa.Reverse();
+                foreach (int x in subsecventa)
+                    Console.Write(x + ", ");
+            }
+            
+            TipLCS(vector, vector1, LCS(vector, vector1));
+            Console.ReadLine();
+        }
+    }
+}
